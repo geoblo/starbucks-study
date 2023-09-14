92dcbbff2331cb98ed54bb3ca77bd0d0488a90d4
geoblo <geoblo@naver.com>
2023-09-14 17:03:33 +0900

    업로드

--- js/main.js
+++ js/main.js
@@ -76,3 +76,19 @@ new Swiper('.notice .swiper', {
   loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 4 -> 다시 1
   autoplay: true // 자동 재생 여부
 });
+
+// 프로모션 수평 슬라이드 기능
+new Swiper('.promotion .swiper', {
+  direction: 'horizontal', // 수평 슬라이드(기본값)
+  loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 4 -> 다시 1
+  autoplay: { // 자동 재생 여부
+    delay: 5000 // 5초마다 슬라이드 바뀜(기본값: 3000)
+  },
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수(기본값: 1)
+  spaceBetween: 10, // 슬라이드 사이 여백(간격) px
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  pagination: { // 페이지 번호 사용
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+});
