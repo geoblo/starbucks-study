eb722ba7528788e8d79b7cb9ab629c3468c75ae2
geoblo <geoblo@naver.com>
2023-09-15 16:50:38 +0900

    업로드

--- js/main.js
+++ js/main.js
@@ -122,3 +122,24 @@ promotionToggleBtn.addEventListener('click', function () {
 // 유튜브 섹션 위에 부유 요소 애니메이션 처리
 // gsap.to(요소, 지속시간, 옵션: {})
 // 옵션 참고: https://greensock.com/docs/v3/GSAP/gsap.to()
+gsap.to('.floating1', 1.5, {
+  delay: 1, // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정
+  y: 15, // 수직으로 얼마나 움직일지 설정(translateY(수치); 와 같음)
+  repeat: -1, // 몇 번 반복하는지를 설정, -1은 무한 반복
+  yoyo: true, // 한 번 재생된 애니메이션을 다시 뒤로 재생
+  ease: Power1.easeInOut // 타이밍 함수 적용, 느리게-빠르게-느리게
+});
+gsap.to('.floating2', 2, {
+  delay: 0.5,
+  y: 15,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+});
+gsap.to('.floating3', 2.5, {
+  delay: 1.5,
+  y: 20,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+});
